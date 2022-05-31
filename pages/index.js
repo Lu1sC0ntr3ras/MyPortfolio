9d910b158b204022993474939322b80941fee35b
camilo112 <camiloroyero99@hotmail.com>
2022-05-31 08:38:20 -0500

    My first commit :v

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Welcome my page</title>
+        <link rel="icon" href="/Logo.png" />
+      </Head>
+    <h1>Hello, my name is Luis and this page is my future portfolio 😀</h1>
+    </div>
+  )
+}
