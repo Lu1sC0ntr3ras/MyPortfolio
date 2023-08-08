861792c2a333901759ef2cfd9337e0c9f1c0b5c1
Lu1sC0ntr3ras <LuisE.Contreras@outlook.com>
2023-08-08 16:36:39 -0500

    start project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { Fragment } from 'react';
+
+const Home: NextPage = () => {
+  return (
+    <Fragment>
+      <h1>Hello world</h1>
+    </Fragment>
+  );
+};
+
+export default Home;
