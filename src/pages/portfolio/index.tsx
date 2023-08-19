f42afbd49b5e89cfc6e8b8235a61cc4f379be1e6
Lu1sC0ntr3ras <LuisE.Contreras@outlook.com>
2023-08-19 12:20:47 -0500

    added new profile

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,21 +1,56 @@
+import { projects } from '@/data/constant/portfolio_projects';
 import { NextPage } from 'next';
 
-const Home: NextPage = () => {
+const Portfolio: NextPage = () => {
+  const Buttons = ({ data }: { data: string[]; }): JSX.Element => {
+    return (
+      <div style={{ display: 'flex', gap: '10px' }} >
+        {
+          data.map((len, index) => {
+            return (
+              <div
+                style={{
+                  width: 'fixed',
+                  border: '1px solid red',
+                  padding: '0px 3px',
+                  cursor: 'pointer',
+                  borderRadius: '3px'
+                }}
+                key={len + index}
+              >
+                <p className='text fs-12'>{len}</p>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  };
+
   return (
     <div className='page_light'>
       <p className={'title_light fs-30'}>{'PORTFOLIO'}</p>
+      <Buttons data={projects.lenguajes} />
+      <Buttons data={projects.frontend} />
+      <Buttons data={projects.backend} />
       <div
         style={{
           width: '100%',
-          height: '100%',
           border: '1px solid red'
         }}
-        className=''
       >
-        <p>asdasd</p>
+        {
+          projects.projects.map((obj, index) => {
+            return (
+              <div>
+
+              </div>
+            );
+          })
+        }
       </div>
     </div>
   );
 };
 
-export default Home;
+export default Portfolio;
--- src/shared/components/base/sidebar/Sidebar.component.tsx
+++ src/shared/components/base/sidebar/Sidebar.component.tsx
@@ -26,63 +26,30 @@ const Sidebar: FC<ISidebar> = ({ leftOptions, subtitle, title }) => {
   }, [router.pathname]);
 
   return (
-    // <div className={`${first.current ? 'first_sidebar' : 'sidebar'}`} >
-    <div className={`${first.current ? 'first_sidebar' : open ? 'sidebar' : 'sidebar_bottom'}`} >
-      <div className={open ? 'left_bar' : 'left_bar_closed'} >
-        <div className='open_bar'
-          onClick={(event) => openLeftBar(event)}
-        />
-        <div className='left_text' >
-          <div className='container_left_titles'>
-            <p className='title_light'>{title}</p>
-            <p className='text_light' >{subtitle}</p>
-          </div>
-          <div className='options'>
-            {leftOptions.map((opt, index) => {
-              const active: boolean = opt.route === leftOption || router.pathname.includes(opt.route ?? '');
-              return (
-                <p
-                  key={index + opt.text}
-                  className={`text_light pointer`}
-                  onClick={() => openRightBar(opt)}
-                >
-                  {active ? `=>  ${opt.text} <=` : opt.text}
-                </p>
-              );
-            })}
-          </div>
+    <div className={first.current ? 'first_left_bar' : open ? 'left_bar' : 'left_bar_closed'} >
+      <div className='open_bar'
+        onClick={(event) => openLeftBar(event)}
+      />
+      <div className='left_text' >
+        <div className='container_left_titles'>
+          <p className='title_light'>{title}</p>
+          <p className='text_light' >{subtitle}</p>
+        </div>
+        <div className='options'>
+          {leftOptions.map((opt, index) => {
+            const active: boolean = opt.route === leftOption || router.pathname.includes(opt.route ?? '');
+            return (
+              <p
+                key={index + opt.text}
+                className={`text_light pointer`}
+                onClick={() => openRightBar(opt)}
+              >
+                {active ? `=>  ${opt.text} <=` : opt.text}
+              </p>
+            );
+          })}
         </div>
       </div>
-      {/* <div className={`${second.current && !rightOption.length
-        ? 'first_right_bar'
-        : rightOption.length && open
-          ? 'right_bar'
-          : 'right_bar_closed'}`}
-      >
-        <div className={rightOption.length && open ? 'right_bar_text' : 'right_bar_text_close'}
-          style={{
-            display: rightOption.length && open ? 'flex' : 'none'
-          }}
-        >
-          {
-            rightOption.map((opt, index) => {
-              const active: boolean = router.pathname.includes(opt.route ?? '');
-              return (
-                <p
-                  key={index + opt.text}
-                  className={`text_light`}
-                  onClick={() => {
-                    router.push(`${opt.route}`);
-                    setOpen(false);
-                  }}
-                >
-                  {active ? `=>  ${opt.text} <=` : opt.text}
-                </p>
-              );
-            })
-          }
-        </div>
-      </div> */}
     </div>
   );
 };
